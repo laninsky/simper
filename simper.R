fcebf1f2c56be3d0ebca0262672c30bfaedc42db
Alana Alexander <laninsky@gmail.com>
2016-12-28 09:51:37 -0600

    Update simper.R

--- simper.R
+++ simper.R
@@ -22,30 +22,23 @@ if (length(zerorows)>0) {
 
 sp_output <- matrix(NA,ncol=7,nrow=(dim(sp_data)[2]))
 sp_output[,1] <- names(sp_data)
-gp_row <- which(names(gp_data)==group_name)
+gp_col <- which(names(gp_data)==group_name)
 gp_names <- unique(gp_data[,gp_row])
 
-#see whether it can be vectorized and made quicker.
-
+sp_data_1 <- sp_data[(which(gp_data[,gp_col]==gp_names[1])),]
+sp_data_2 <- sp_data[(which(gp_data[,gp_col]==gp_names[2])),]
 
 for (i in 1:(dim(sp_data)[2])) {
   temp_sp <- NULL
-  for (j in 1:((dim(sp_data)[1])-1)) {
-  print(paste("up to row",j,"of",dim(sp_data)[1],"in total"))
-  flush.console()
-    for (k in (j+1):(dim(sp_data)[1])) {
-      if(gp_data[j,gp_row]==gp_data[k,gp_row]) {
-        next
-      } else {
-        bray_curt <- abs(sp_data[j,i]-sp_data[k,i])/(sp_data[j,i]+sp_data[k,i])
-        temp_sp <- c(temp_sp,bray_curt)
-      }
-    }
+  for (j in 1:(dim(sp_data_1)[1])) {
+  bray_curt <- (abs(sp_data_2[,i]-sp_data_1[j,i])/(sp_data_2[,i]+sp_data_1[j,i]))
+  bray_curt <- bray_curt[!is.na(bray_curt)]
+  temp_sp <- c(temp_sp,bray_curt)
   }
   sp_output[i,4] <- mean(temp_sp,na.rm=TRUE)
   sp_output[i,5] <- mean(temp_sp,na.rm=TRUE)/sd(temp_sp,na.rm=TRUE)
-  sp_output[i,2] <- mean(sp_data[(which(gp_data[,1]==gp_names[1])),i],na.rm=TRUE)
-  sp_output[i,3] <- mean(sp_data[(which(gp_data[,1]==gp_names[2])),i],na.rm=TRUE)   
+  sp_output[i,2] <- mean(sp_data_1[,i],na.rm=TRUE)
+  sp_output[i,3] <- mean(sp_data_2[,i],na.rm=TRUE)   
   print(paste("done with species",i))
   flush.console()
 }
