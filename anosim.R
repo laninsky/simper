047eaa46c7901ebb569b87d97c02ea84c26515f5
Alana Alexander <laninsky@gmail.com>
2016-12-30 00:01:00 -0600

    Update anosim.R

--- anosim.R
+++ anosim.R
@@ -18,7 +18,9 @@ if (length(zerorows)>0) {
   gp_data <- sp_data[-zerorows,]
 }
 
-sp_output <- NULL
+sp_output_between <- NULL
+sp_output_within1 <- NULL
+sp_output_within2 <- NULL
 gp_col <- which(names(gp_data)==group_name)
 gp_names <- unique(gp_data[,gp_col])
 
@@ -30,7 +32,22 @@ sp_data_2 <- data.matrix(sp_data_2)
 for (j in 1:(dim(sp_data_1)[1])) {
   temp_sp <- rowSums(abs(sweep(sp_data_2, 2, sp_data_1[j,], "-")))/(rowSums(sp_data_2)+sum(sp_data_1[j,]))
   temp_sp <- matrix(temp_sp,ncol=1)
-  sp_output <- rbind(sp_output,temp_sp)
+  sp_output_between <- rbind(sp_output_between,temp_sp)
  }
-  
+write.table(sp_output_between,"sp_output_between",quote=FALSE,col.names=FALSE,row.names=FALSE)
+rm(sp_output_between)
+
+for (j in 1:(dim(sp_data_1)[1])) {
+  temp_sp <- rowSums(abs(sweep(sp_data_1[-j,], 2, sp_data_1[j,], "-")))/(rowSums(sp_data_1[-j,])+sum(sp_data_1[j,]))
+  temp_sp <- matrix(temp_sp,ncol=1)
+  sp_output_within1 <- rbind(sp_output_within1,temp_sp)
+ }
+write.table(sp_output_within1,"sp_output_within1",quote=FALSE,col.names=FALSE,row.names=FALSE)
+rm(sp_output_within1)
+
+for (j in 1:(dim(sp_data_2)[1])) {
+  temp_sp <- rowSums(abs(sweep(sp_data_2[-j,], 2, sp_data_2[j,], "-")))/(rowSums(sp_data_2[-j,])+sum(sp_data_2[j,]))
+  temp_sp <- matrix(temp_sp,ncol=1)
+  sp_output_within2 <- rbind(sp_output_within2,temp_sp)
+ }
   
