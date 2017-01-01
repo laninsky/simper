86c28d94f0df6e14d53afccd550d8fb780f03e36
Alana Alexander <laninsky@gmail.com>
2016-12-31 22:56:34 -0600

    Update anosim.R

--- anosim.R
+++ anosim.R
@@ -53,8 +53,6 @@ for (j in 1:((dim(sp_data_1)[1])-1)) {
 write.table(sp_output_within1,"sp_output_within1",quote=FALSE,col.names=FALSE,row.names=FALSE)
 rm(sp_output_within1)
 
-#
-
 for (j in 1:(dim(sp_data_2)[1])) {
   if(!(is.null(dim(sp_data_2[-1:-j,])[1]))) {
     temp_sp <- rowSums(abs(sweep(sp_data_2[-1:-j,], 2, sp_data_2[j,], "-")))/(rowSums(sp_data_2[-1:-j,])+sum(sp_data_2[j,]))
@@ -66,6 +64,8 @@ for (j in 1:(dim(sp_data_2)[1])) {
     sp_output_within2 <- rbind(sp_output_within2,temp_sp)
     }
  }
+
+#
 
 temp1 <- rep("within",(dim(sp_output_within2)[1]))
 sp_output_within2 <- cbind(temp1,sp_output_within2)  
